2f35adbc627d9d811f4794a4baaad540ad6c4cbb
Edmond Leung <leung.edmond@gmail.com>
2015-04-19 13:26:06 +1000

    Added Sentry transport to winston

--- logger.js
+++ logger.js
@@ -1,10 +1,15 @@
 import winston from 'winston';
+import Sentry from 'winston-sentry';
 
-export default new (winston.Logger)({
-  transports: [
-    new (winston.transports.Console)({
-      colorize: true,
-      timestamp: true,
-    }),
-  ]
-});
+const transports = [
+  new (winston.transports.Console)({
+    colorize: true,
+    timestamp: true,
+  }),
+];
+
+if (process.env.SENTRY_DSN) {
+  transports.push(new Sentry({ dsn: process.env.SENTRY_DSN }));
+}
+
+export default new (winston.Logger)({ transports });
