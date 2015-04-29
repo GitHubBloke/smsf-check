03adfdbff2f210dde9af9ff5c4e407ed36d09936
Edmond Leung <leung.edmond@gmail.com>
2015-04-29 17:08:21 +1000

    Added host to email locals

--- keystone.js
+++ keystone.js
@@ -57,16 +57,23 @@ keystone.set('routes', require('./routes'));
 // default email templates, you may remove them if you're using your own.
 
 keystone.set('email locals', {
-  'logo_src': '/images/logo-email.gif',
-  'logo_width': 194,
-  'logo_height': 76,
-  'theme': {
-    'email_bg': '#f9f9f9',
-    'link_color': '#2697de',
-    'buttons': {
-      'color': '#fff',
-      'background_color': '#2697de',
-      'border_color': '#1a7cb7',
+  utils: keystone.utils,
+  host: (function() {
+    if (keystone.get('env') === 'staging') return 'http://smsf-check.herokuapp.com';
+    if (keystone.get('env') === 'production') return 'http://www.superiq.com/smsf-check';
+    return (keystone.get('host') || 'http://localhost:') + (keystone.get('port') || '3000');
+  })(),
+
+  logo_src: '/images/logo-email.gif',
+  logo_width: 194,
+  logo_height: 76,
+  theme: {
+    email_bg: '#f9f9f9',
+    link_color: '#2697de',
+    buttons: {
+      color: '#fff',
+      background_color: '#2697de',
+      border_color: '#1a7cb7',
     },
   },
 });
