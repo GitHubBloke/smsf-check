bdd6932918e381d79e7c1885ed207338933bf678
Edmond Leung <leung.edmond@gmail.com>
2015-04-29 12:33:37 +1000

    Added initial terms and privacy footer links

--- assets/js/components/Footer.js
+++ assets/js/components/Footer.js
@@ -10,6 +10,10 @@ export default class Footer extends Component {
           <small className='text-muted'>
             &copy; {new Date().getFullYear()} SuperIQ. All rights reserved.
           </small>
+          <ul className='list-inline pull-right small prepend-xs-1 append-xs-none'>
+            <li><a href='#'>Terms &amp; Conditions</a></li>
+            <li><a href='#'>Privacy Policy</a></li>
+          </ul>
         </div>
       </footer>
     );
