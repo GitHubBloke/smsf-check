815b57de0102f1352aca0c4eec9527c6d252f8fc
Edmond Leung <leung.edmond@gmail.com>
2015-04-29 19:35:05 +1000

    Make sure app doc title uses locals too

--- assets/js/App.js
+++ assets/js/App.js
@@ -3,11 +3,12 @@ import React, { Component } from 'react';
 import { RouteHandler } from 'react-router';
 
 import Footer from './components/Footer';
+import locals from '../utils/locals';
 
 export default class App extends Component {
   render() {
     return (
-      <DocumentTitle title='SMSF Health Check'>
+      <DocumentTitle title={locals.name}>
         <div>
           <RouteHandler/>
           <Footer/>
