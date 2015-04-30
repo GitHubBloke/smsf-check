684799857d5249eebdbe3deffd9837d829b8faec
Edmond Leung <leung.edmond@gmail.com>
2015-04-30 21:09:16 +1000

    Added fund name and abn to model

--- models/User.js
+++ models/User.js
@@ -7,20 +7,18 @@ const User = new keystone.List('User');
 User.add({
   name: { type: Types.Name, required: true, index: true },
   email: { type: Types.Email, initial: true, required: true, index: true },
-  password: { type: Types.Password, initial: true, required: true }
+  password: { type: Types.Password, initial: true, required: true },
+  fund: {
+    name: { type: String, required: true, initial: true },
+    abn: { type: String, requied: true, initial: true },
+  },
 }, 'Permissions', {
   isAdmin: { type: Boolean, label: 'Can access Keystone', index: true }
 });
 
-// Provide access to Keystone
 User.schema.virtual('canAccessKeystone').get(function() {
   return this.isAdmin;
 });
 
-
-/**
- * Registration
- */
-
-User.defaultColumns = 'name, email, isAdmin';
+User.defaultColumns = 'name, email, fund, isAdmin';
 User.register();
