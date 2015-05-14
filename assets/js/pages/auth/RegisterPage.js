b5e84e024f3bbf1b272c5e035fa7de4d9a6de102
Edmond Leung <leung.edmond@gmail.com>
2015-05-14 18:40:52 +1000

    Made rendering more modular for register page

--- assets/js/pages/auth/RegisterPage.js
+++ assets/js/pages/auth/RegisterPage.js
@@ -45,58 +45,12 @@ class RegisterPage extends Validatable {
     return (
       <DocumentTitle title={`${locals.name} - ${this.formatMessage(this.getIntlMessage('register.title'))}`}>
         <div>
-          <div className='hero text-inverse'>
-            <Grid>
-              <Row className='text-center clearfix'>
-                <Col md={10} mdOffset={3} className='prepend-xs-2 append-xs-2 prepend-md-5 append-md-5'>
-                  <h1 className='prepend-xs-tiny'>{locals.name}</h1>
-                  <h3 className='text-normal'>
-                    <FM message={this.getIntlMessage('home.hero.intro')}
-                      name={locals.name}
-                      brand={locals.brand} />
-                  </h3>
-                  <div className='prepend-xs-1'>
-                    <Button className='btn-clear btn--wide' bsSize='large' href='/#register'>
-                      <FM message={this.getIntlMessage('home.hero.signup.actionLabel')} />
-                    </Button>
-                  </div>
-                </Col>
-              </Row>
-            </Grid>
-          </div>
+          {this.renderHero()}
 
           <Grid>
-            <Row className='text-center prepend-xs-2 prepend-sm-3'>
-              <Col md={4} mdOffset={2} className='append-xs-2 append-sm-3'>
-                <IconStack size='3'>
-                  <Icon id='record' size='2' stacked className='text-primary' />
-                  <Icon id='android-lock' size='1' stacked className='text-inverse' />
-                </IconStack>
-                <div className='prepend-xs-1'>
-                  <FM message={this.getIntlMessage('home.features.safeData')} />
-                </div>
-              </Col>
-              <Col md={4} className='append-xs-2 append-sm-3'>
-                <IconStack size='3'>
-                  <Icon id='record' size='2' stacked className='text-primary' />
-                  <Icon id='information' size='1' stacked className='text-inverse' />
-                </IconStack>
-                <div className='prepend-xs-1'>
-                  <FM message={this.getIntlMessage('home.features.greatAdvice')} />
-                </div>
-              </Col>
-              <Col md={4} className='append-xs-2 append-sm-3'>
-                <IconStack size='3'>
-                  <Icon id='record' size='2' stacked className='text-primary' />
-                  <Icon id='stats-bars' size='1' stacked className='text-inverse' />
-                </IconStack>
-                <div className='prepend-xs-1'>
-                  <FM message={this.getIntlMessage('home.features.compare')} name={locals.name} />
-                </div>
-              </Col>
-            </Row>
+            {this.renderFeatures()}
 
-            <Row className=''>
+            <Row>
               <Col md={12} mdOffset={2} className='append-xs-2'>
                 <Well bsSize='large'>
                   {registeredUser ? this.renderSuccess() : this.renderForm()}
@@ -106,6 +60,64 @@ class RegisterPage extends Validatable {
           </Grid>
         </div>
       </DocumentTitle>
+    );
+  }
+
+  renderHero() {
+    return (
+      <div className='hero text-inverse'>
+        <Grid>
+          <Row className='text-center clearfix'>
+            <Col md={10} mdOffset={3} className='prepend-xs-2 append-xs-2 prepend-md-5 append-md-5'>
+              <h1 className='prepend-xs-tiny'>{locals.name}</h1>
+              <h3 className='text-normal'>
+                <FM message={this.getIntlMessage('home.hero.intro')}
+                  name={locals.name}
+                  brand={locals.brand} />
+              </h3>
+              <div className='prepend-xs-1'>
+                <Button className='btn-clear btn--wide' bsSize='large' href='/#register'>
+                  <FM message={this.getIntlMessage('home.hero.signup.actionLabel')} />
+                </Button>
+              </div>
+            </Col>
+          </Row>
+        </Grid>
+      </div>
+    );
+  }
+
+  renderFeatures() {
+    return (
+      <Row className='text-center prepend-xs-2 prepend-sm-3'>
+        <Col md={4} mdOffset={2} className='append-xs-2 append-sm-3'>
+          <IconStack size='3'>
+            <Icon id='record' size='2' stacked className='text-primary' />
+            <Icon id='android-lock' size='1' stacked className='text-inverse' />
+          </IconStack>
+          <div className='prepend-xs-1'>
+            <FM message={this.getIntlMessage('home.features.safeData')} />
+          </div>
+        </Col>
+        <Col md={4} className='append-xs-2 append-sm-3'>
+          <IconStack size='3'>
+            <Icon id='record' size='2' stacked className='text-primary' />
+            <Icon id='information' size='1' stacked className='text-inverse' />
+          </IconStack>
+          <div className='prepend-xs-1'>
+            <FM message={this.getIntlMessage('home.features.greatAdvice')} />
+          </div>
+        </Col>
+        <Col md={4} className='append-xs-2 append-sm-3'>
+          <IconStack size='3'>
+            <Icon id='record' size='2' stacked className='text-primary' />
+            <Icon id='stats-bars' size='1' stacked className='text-inverse' />
+          </IconStack>
+          <div className='prepend-xs-1'>
+            <FM message={this.getIntlMessage('home.features.compare')} name={locals.name} />
+          </div>
+        </Col>
+      </Row>
     );
   }
 
