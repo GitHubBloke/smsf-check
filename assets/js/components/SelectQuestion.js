d2d5893180cba87875dbb2707c459d070fb00031
Edmond Leung <leung.edmond@gmail.com>
2015-05-22 02:18:33 +1000

    Import Select.js inside react-select directly

--- assets/js/components/SelectQuestion.js
+++ assets/js/components/SelectQuestion.js
@@ -1,7 +1,7 @@
 import _ from 'lodash';
 import classNames from 'classnames';
 import React, { PropTypes } from 'react';
-import Select from 'react-select';
+import Select from 'react-select/lib/Select';
 
 import BaseComponent from '../utils/BaseComponent';
 
